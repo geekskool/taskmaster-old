2799508807039a402ad8f059181c7b3fc10220ee
sankalp0o <sankalpagarwal294@gmail.com>
2016-08-04 14:10:01 +0530

    get, post on user, task working, added nodemon, exported read in graphology

--- server/controller/task.js
+++ server/controller/task.js
@@ -1,21 +1,73 @@
+const graph = require('../../graphology/lib/index')
+global.graphDataPath = __dirname;
 const task = {}
-task.handleGet = function(req,res,next){
-    var task = {"task":"project","id":"123"}
-    res.send(task)
+
+function createTask(task){
+	graph.load();
+	const taskObject = {}
+	taskObject.title = task.title;
+	taskObject.status = true;
+	taskObject.comments = '';
+	taskObject.date = task.date;
+	taskObject.assgnByName = task.assgnByName
+	taskObject.assgnByPhon = task.assgnByPhon;
+	taskObject.assgnToName = task.assgnToName
+	taskObject.assgnToPhon = task.assgnToPhon;
+
+	const taskNode = new graph.Node('task', taskObject);
+
+	let query = new graph.Query(graph.find('phone', task.assgnByPhon));
+	var assignee = query.next();
+	console.log(assignee);
+
+	let query2 = new graph.Query(graph.find('phone', task.assgnToPhon));
+	var assigner = query2.next();
+	console.log(assigner);
+
+	taskNode.addEdge('by', assignee);
+	taskNode.addEdge('to', assigner);
+	graph.save();
 
 }
+
+
+function getTasks(userId){
+    graph.load();
+    var user = new graph.Query(graph.find('phone', userId));
+ 
+    var temp = user.next().in;
+    var result = [];
+
+    for (var key in temp){
+    	if (temp[key].type!=='child'){
+    		var taskId = temp[key].in;
+    		result.push(graph.read(taskId).data);
+    	}
+    }
+    return result;
+}
+
+task.handleGet = function(req,res,next){
+//    var task = {"task":"project","id":"123"}
+	var id = req.params.phonenm
+    console.log(id);
+    let list = getTasks(id);
+    res.send(list);
+
+}
+
 task.handlePost = function(req,res,next){
-	var tasks = new Object()
-	tasks.id = req.body.id
-	tasks.name = req.body.name
-	tasks.assgnByName = req.body.assngByName
-	tasks.assgnByPhon = req.body.assngByphon
-	tasks.assgnToName = req.body.assgnToName
-	tasks.assgToPhon = req.body.assgnToPhon
-	tasks.date = req.body.date
-	tasks.status = req.body.status
-	tasks.comment = req.body.comment
-	console.log(tasks)
+	var newTask = new Object()
+
+	newTask.title = req.body.title;
+	newTask.assgnByName = req.body.assgnByName;
+	newTask.assgnByPhon = req.body.assgnByPhon;
+	newTask.assgnToName = req.body.assgnToName;
+	newTask.assgnToPhon = req.body.assgnToPhon;
+	newTask.date = req.body.date;
+
+	console.log(newTask);
+	createTask(newTask);
 	res.status(200).json({
 		message:"Task created succussfully"
 	})
--- server/controller/user.js
+++ server/controller/user.js
@@ -1,24 +1,63 @@
-
-
-
+const graph = require('../../graphology/lib/index')
+global.graphDataPath = __dirname;
 const user = {}
+
+
+//const god = new graph.Node('god', {power: 'over9000'}); //do this only once
+//have to create a query for god like god = query(power, 'over9000')
+
+
+function createNewUser(user){
+    graph.load();
+    var query = new graph.Query(graph.find('power', 'over9000'));
+    var god = query.next();
+    console.log('god', god);
+    console.log('user',user);
+    const user1 = new graph.Node('user', {
+     name: user.name,
+     phone: user.phoneNum,
+    })
+    god.addEdge('child', user1);
+    graph.save();
+
+}
+
+
+function getUsers(userId){
+    graph.load();
+    var god = new graph.Query(graph.find('power', 'over9000'));
+ 
+    var temp = god.next().out;
+    var result = [];
+
+    for (var key in temp){
+        var userId =  temp[key].out; //should be id
+        result.push(graph.read(userId).data);
+    }
+    return result; 
+
+    //remove authorisation token
+    //Need to filter out the user himself from his id
+
+}
 
 user.handlePost = function(req,res,next){
     var user = new Object()
     user.name = req.body.name
     user.phoneNum = req.body.phonenm
-    console.log(user)
-    
+    console.log(req.body);
+    createNewUser(user)
     res.status(200).json({
      	message: "User created succussfully"
     })
 }
 
-user.handleGet = function(req,res,next){
-    var user = {"username":"pooja","phone":"1233244"}
-    var id = req.params.phone
 
-    res.send(user)
+user.handleGet = function(req,res,next){
+    var id = req.params.phonenm
+    console.log(id);
+    let list = getUsers(id);
+    res.send(list);
 
 }
 
