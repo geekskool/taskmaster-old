6beff351c61cdaf6b9496a62faa966d86ab91c92
Shruti Kale <shrutick05@gmail.com>
2016-09-30 11:44:31 +0530

    Updated addRow to IO-square

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -9,115 +9,166 @@ var assignName
 var assignNum
 var taskObj
 
-var modal = document.getElementById('myModal')
-var span = document.getElementsByClassName('close')[0]
-
-window.onload = function () {
-  span.onclick = function () {
-    modal.style.display = 'none'
-  }
-}
-
-jQuery(function ($) {
-  var $chatbox = $('#chatbox')
-  var $msg = $('#usermsg')
-  var $form = $('#sendMsg')
-  var userTo
-  $form.submit(function (e) {
-    e.preventDefault()
-    if (user.name == taskObj.data.assgnToName) {
-      userTo = taskObj.data.assgnByName
-    } else {
-      userTo = taskObj.data.assgnToName
-    }
-
-    $chatbox.append('<div class="self"><b>' + user.name + '</b>' + ': ' + $msg.val() + '</div>')
-
-    taskObj.data.comments = '<b>' + user.name + '</b>' + ': ' + $msg.val() + '<br>'
-
-    socket.emit('sendmessage', { msg: $msg.val(), to: userTo })
-    putComment(taskObj)
-
-    console.log(`${taskObj.data.assgnToName} <--name`)
-    console.log(taskObj)
-
-    $msg.val(' ')
-  })
-
-  // socket.on("new message", function(data) {
-  //     $chatbox.append('<b>' + data.name + '</b>' + ": " + data.msg + '<br>')
-
-  // })
-
-  // socket.on("private", function(data) {
-  //     $chatbox.append('<b>' + data.name + '</b>' + ": " + data.msg + '<br>')
-
-  // })
-
-  socket.on('discuss', function (data) {
-    console.log(`${data} <--discuss on event`)
-    $chatbox.append('<div class="other"><b>' + data.name + '</b>' + ': ' + data.msg + '</div>')
-  })
-})
-
 var socket = io()
+var chatModal = document.getElementById('chat') // chat modal
+var chatBox = document.getElementById('chatbox') // chat bos
+var closeChat = document.getElementsByClassName('close')[0] // close button
 
-socket.on('connect', function () {
-  console.log('connected to server')
-  socket.emit('joinroom', user.name)
-})
+// var modal = document.getElementById('myModal')
 
-socket.on('notify', function (data) {
-  alert('you have a new task from  ' + data)
-})
+// window.onload = function () {
+closeChat.onclick = function () {
+  chatModal.style.display = 'none'
+}
+// }
 
-// getUsers(populateUsers)
-getUsers()
+// Execution starts from here
+/* IO to get other users who can be assigned tasks by the currently logged in
+   user */
+IO.getJSON('/api/users/' + user.phone)
+  .then(function (users) {
+    userList = users
+    populateUserList(userList)
+  })
 
-function httpRequest (method, url, callback) {
-  console.log('inside httpRequest')
-  var request = new XMLHttpRequest()
-  request.open(method, url, true)
-  request.setRequestHeader('content-type', 'application/json')
-  request.onreadystatechange = function () {
-    if (request.readyState == 4 && request.status == 200) {
-      console.log(request)
-      callback(request)
-    }
+// function to fetch the list of users and render it in the HTML list
+function populateUserList () {
+  var assign = document.getElementById('assignTo')
+  for (var i = 0; i < userList.length; i++) {
+    var option = document.createElement('option')
+    option.setAttribute('value', userList[i].name)
+    option.innerText = userList[i].name
+    assign.appendChild(option)
   }
-  return request
-}
-
-function httpGet (url, callback) {
-  var request = httpRequest('GET', url, function (request) {
-    callback(request.responseText)
-  })
-  request.send()
-}
-
-function httpPost (url, body, callback) {
-  var request = httpRequest('POST', url, function (request) {
-    console.log('inside httpPost')
-    callback(request)
-  })
-  console.log(request)
-  request.send(JSON.stringify(body))
-}
-
-function getUsers () {
-  httpGet('/api/users/' + user.phone, function (rt) {
-    // console.log(rt)
-    userList = JSON.parse(rt)
-    populateUsers()
-  })
+  getTasks()
 }
 
 function getTasks () {
-  httpGet('/api/tasks/' + user.phone, function (rt) {
-    // console.log(rt)
-    taskList = JSON.parse(rt)
-    addView()
+  // IO to fetch the tasks object of a user
+  IO.getJSON('/api/tasks/' + user.phone)
+    .then(function (taskList) {
+      addView(taskList)
+    })
+}
+
+function addView (taskList) {
+  for (var i = 0; i < taskList.length; i++) {
+    if (taskList[i].data.status == true)
+      addRow(taskList[i])
+  }
+}
+
+function addRow (task) {
+  var row = document.getElementById('tasks').insertRow()
+  var taskname = row.insertCell(0)
+  var owner = row.insertCell(1)
+  var byDate = row.insertCell(2)
+  var done = row.insertCell(3)
+  var discuss = row.insertCell(4)
+
+  taskname.innerText = task.data.title
+  owner.innerText = task.data.assgnToName
+  byDate.innerText = task.data.date.slice(0, 10)
+
+  var iconDone = document.createElement('i')
+  iconDone.setAttribute('class', 'small material-icons')
+  iconDone.innerHTML = 'done'
+
+  var donebutton = document.createElement('button')
+  donebutton.setAttribute('class', 'button')
+  done.appendChild(donebutton)
+  donebutton.appendChild(iconDone)
+  // event istener for do
+  IO.click(donebutton)
+    .then(function (e) {
+      deleteTask(task)
+    })
+
+  var iconDiscuss = document.createElement('i')
+  iconDiscuss.setAttribute('class', 'small material-icons')
+  iconDiscuss.innerHTML = 'chat_bubble'
+
+  var discussbutton = document.createElement('button')
+  discussbutton.setAttribute('class', 'button')
+  discussbutton.addEventListener('click', function () {
+    chatModal.style.display = 'block' // display chat modal
+    chatBox.innerHTML = null
+    taskObj = task
+  // getComment(task)
   })
+  discuss.appendChild(discussbutton)
+  discussbutton.appendChild(iconDiscuss)
+
+  // code to get chat box
+  IO.click(discussbutton)
+    .bind(function (event) {
+      return new IO.getJSON('/api/comment/' + task.id)
+    })
+    .then(function (event, comments) {
+      chatModal.style.display = 'block' // display chat modal
+      chatBox.innerHTML = null
+
+      taskObj = task
+
+      for ( var i = 0; i < comments.length; i++) // render previous comments
+      {
+        displayComment(comments[i])
+      }
+    })
+}
+
+function displayComment (comment) {
+  var msg = document.createElement('div') // create a new div
+  msg.innerHTML = comment.sentBy + ' ' + comment.time + ' ' + comment.message
+
+  if (comment.sentBy === user.name) {
+    msg.setAttribute('class', 'self')
+  } else {
+    msg.setAttribute('class', 'other')
+  }
+  chatBox.appendChild(msg) // creates new div for msg inside the chatbox
+}
+
+function deleteTask (task) {
+  task.data.status = false
+
+  var update = new XMLHttpRequest()
+  update.open('PUT', '/api/tasks/', true)
+  update.setRequestHeader('content-type', 'application/json')
+  update.onreadystatechange = function () {
+    if (update.readyState == 4 && update.status == 200) {
+      getTasks()
+      location.reload(true)
+    }
+  }
+  update.send(JSON.stringify(task))
+}
+
+// function getComment (task) {
+//   var id = task.id
+
+//   httpGet('/api/comment/' + id, function (rt) {
+//     appendComment(rt)
+//   })
+// }
+
+function putComment (task) {
+  var comment = new XMLHttpRequest()
+  comment.open('PUT', '/api/comment/', true)
+  comment.setRequestHeader('content-type', 'application/json')
+  comment.onreadystatechange = function () {
+    if (comment.readyState == 4 && comment.status == 200) {
+      console.log('function executed')
+    }
+  }
+  comment.send(JSON.stringify(task))
+}
+var $chatbox = $('#chatbox')
+
+function appendComment (comment) {
+  chatModal.style.display = 'block'
+  console.log(comment)
+  $chatbox.append(comment)
 }
 
 function assignTo () {
@@ -167,6 +218,77 @@ function createTask () {
   })
 }
 
+jQuery(function ($) {
+  var $chatbox = $('#chatbox')
+  var $msg = $('#usermsg')
+  var $form = $('#sendMsg')
+  var userTo
+  $form.submit(function (e) {
+    e.preventDefault()
+    if (user.name == taskObj.data.assgnToName) {
+      userTo = taskObj.data.assgnByName
+    } else {
+      userTo = taskObj.data.assgnToName
+    }
+
+    $chatbox.append('<div class="self"><b>' + user.name + '</b>' + ': ' + $msg.val() + '</div>')
+
+    taskObj.data.comments = '<b>' + user.name + '</b>' + ': ' + $msg.val() + '<br>'
+
+    socket.emit('sendmessage', { msg: $msg.val(), to: userTo })
+    putComment(taskObj)
+
+    console.log(`${taskObj.data.assgnToName} <--name`)
+    console.log(taskObj)
+
+    $msg.val(' ')
+  })
+
+  socket.on('discuss', function (data) {
+    console.log(`${data} <--discuss on event`)
+    $chatbox.append('<div class="other"><b>' + data.name + '</b>' + ': ' + data.msg + '</div>')
+  })
+})
+
+socket.on('connect', function () {
+  console.log('connected to server')
+  socket.emit('joinroom', user.name)
+})
+
+socket.on('notify', function (data) {
+  alert('you have a new task from  ' + data)
+})
+
+function httpRequest (method, url, callback) {
+  console.log('inside httpRequest')
+  var request = new XMLHttpRequest()
+  request.open(method, url, true)
+  request.setRequestHeader('content-type', 'application/json')
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      console.log(request)
+      callback(request)
+    }
+  }
+  return request
+}
+
+function httpGet (url, callback) {
+  var request = httpRequest('GET', url, function (request) {
+    callback(request.responseText)
+  })
+  request.send()
+}
+
+function httpPost (url, body, callback) {
+  var request = httpRequest('POST', url, function (request) {
+    console.log('inside httpPost')
+    callback(request)
+  })
+  console.log(request)
+  request.send(JSON.stringify(body))
+}
+
 // function httpGetJSON (url, callback) {
 //   httpGet(url, callback)
 // }
@@ -174,114 +296,3 @@ function createTask () {
 // httpGetJSON('https://api.github.com/', function (responseText) {
 //   console.log(JSON.parse(responseText))
 // })
-
-function populateUsers () {
-  var assign = document.getElementById('assignTo')
-  for (var i = 0; i < userList.length; i++) {
-    var option = document.createElement('option')
-    option.setAttribute('value', userList[i].name)
-    option.innerText = userList[i].name
-    assign.appendChild(option)
-  }
-  getTasks()
-}
-
-function updateTask (task) {
-  task.data.status = false
-
-  var update = new XMLHttpRequest()
-  update.open('PUT', '/api/tasks/', true)
-  update.setRequestHeader('content-type', 'application/json')
-  update.onreadystatechange = function () {
-    if (update.readyState == 4 && update.status == 200) {
-      getTasks()
-      location.reload(true)
-    }
-  }
-  update.send(JSON.stringify(task))
-}
-
-function addView () {
-  for (var i = 0; i < taskList.length; i++) {
-    if (taskList[i].data.status == true)
-      addRow(taskList[i])
-  }
-}
-
-function addRow (task) {
-  var row = document.getElementById('tasks').insertRow()
-  var taskname = row.insertCell(0)
-  var owner = row.insertCell(1)
-  var byDate = row.insertCell(2)
-  var done = row.insertCell(3)
-  var discuss = row.insertCell(4)
-
-  taskname.innerText = task.data.title
-  owner.innerText = task.data.assgnToName
-  byDate.innerText = task.data.date.slice(0, 10)
-
-  // var donebutton = document.createElement('button')
-  // donebutton.innerHTML = 'X'
-  // donebutton.setAttribute('class', 'button')
-  // donebutton.addEventListener('click', function () {
-  //   updateTask(task)
-  // })
-  // done.appendChild(donebutton)
-
-  // Done
-  var iconDone = document.createElement('i')
-  iconDone.setAttribute('class', 'small material-icons')
-  iconDone.innerHTML = 'clear'
-
-  var donebutton = document.createElement('button')
-  // donebutton.innerHTML = 'X'
-  donebutton.setAttribute('class', 'button')
-  donebutton.addEventListener('click', function () {
-    updateTask(task)
-  })
-  done.appendChild(donebutton)
-  donebutton.appendChild(iconDone)
-
-  var iconDiscuss = document.createElement('i')
-  iconDiscuss.setAttribute('class', 'small material-icons')
-  iconDiscuss.innerHTML = 'chat_bubble'
-
-  var discussbutton = document.createElement('button')
-  discussbutton.setAttribute('class', 'button')
-  discussbutton.addEventListener('click', function () {
-    $chatbox.empty()
-    taskObj = task
-    //  console.log(task)
-    getComment(task)
-  //  console.log('Inside Discuss')
-  })
-  discuss.appendChild(discussbutton)
-  discussbutton.appendChild(iconDiscuss)
-}
-
-function getComment (task) {
-  var id = task.id
-
-  httpGet('/api/comment/' + id, function (rt) {
-    appendComment(rt)
-  })
-}
-
-function putComment (task) {
-  var comment = new XMLHttpRequest()
-  comment.open('PUT', '/api/comment/', true)
-  comment.setRequestHeader('content-type', 'application/json')
-  comment.onreadystatechange = function () {
-    if (comment.readyState == 4 && comment.status == 200) {
-      console.log('function executed')
-    }
-  }
-  comment.send(JSON.stringify(task))
-}
-var $chatbox = $('#chatbox')
-
-function appendComment (comment) {
-  modal.style.display = 'block'
-  console.log(comment)
-  $chatbox.append(comment)
-}
